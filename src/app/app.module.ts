8e39c46c9a01b519d744f6b0f1a0b0d9d9e93ab3
Nicolae Bacila <nicolae.bacila@tss-yonder.com>
2018-02-03 12:52:23 +0200

    Add sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppComponent } from './app.component';
 
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {AppComponent} from './app.component';
-
-import {environment} from '../environments/environment';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {StoreModule} from '@ngrx/store';
-
-
-
-import { StoreModule, MetaReducer } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-
+import { environment } from '../environments/environment';
 // not used in production
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { MetaReducer, StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { storeFreeze } from 'ngrx-store-freeze';
 import { PlayerModule } from './player/player.module';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule, MatSidenavModule, MatIconModule, MatButtonModule } from '@angular/material';
 
 
 export const metaReducers: MetaReducer<any>[] = !environment.production
@@ -32,6 +27,7 @@ export const metaReducers: MetaReducer<any>[] = !environment.production
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     StoreModule.forRoot({}, { metaReducers }),
     EffectsModule.forRoot([]),
@@ -40,8 +36,10 @@ export const metaReducers: MetaReducer<any>[] = !environment.production
     RouterModule.forRoot([
       { path: 'album', loadChildren: './album/album.module#AlbumModule' },
       { path: '', pathMatch: 'full', redirectTo: 'album' }
-    ])
-    StoreDevtoolsModule.instrument(),
+    ]),
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
